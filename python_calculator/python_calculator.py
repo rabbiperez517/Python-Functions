57b5346b52026f53f976ab63f18950878c4c69a5
rabbiperez517 <rabbiperez007@gmail.com>
2026-04-27 16:24:39 +0000

    first commit

---
+++ python_calculator/python_calculator.py
@@ -0,0 +1,14 @@
+# A grocery store sells a bag of ice for $1.25 and makes 20% profit. If it sells 500 bags of ice, how muchtot profit does it make
+#Method
+''' Profit = selling price - cost price
+selling price = 1.25
+profit = 20%
+therefore profit on onebag = 20% of 1.25 = 0.25
+profit on 500 bags = 500 * 0.25 = $125'''
+
+# Python code to calculate profit
+selling_price = 1.25
+profit_per_bag = 0.20*1.25
+total_profit = profit_per_bag*500
+print("Total profit made by selling 500 bags of ice is: $", total_profit)
+
